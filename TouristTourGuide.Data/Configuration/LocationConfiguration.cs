54d6928dc8bde42da02067f2e60e40fa58259c06
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2024-04-01 02:18:00 +0300

    add migration

---
+++ TouristTourGuide.Data/Configuration/LocationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TouristTourGuide.Data.Models.Sql.Models;
+namespace TouristTourGuide.Data.Configuration
+{
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.HasData(SetLocation());   
+        }
+
+        private Location SetLocation() 
+        {
+            Location l = new Location()
+            {
+                Id = 1,
+                Country = "UK",
+                City = "London"
+            };
+
+            return l;
+        }
+    }
+}
---
+++ TouristTourGuide.Data/Migrations/20240331231554_locationDataSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TouristTourGuide.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class locationDataSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TouristsTours",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 3, 31, 23, 15, 54, 178, DateTimeKind.Utc).AddTicks(1577),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 3, 31, 23, 0, 27, 398, DateTimeKind.Utc).AddTicks(5137));
+
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "Id", "City", "Country", "Village" },
+                values: new object[] { 1, "London", "UK", null });
+
+            migrationBuilder.UpdateData(
+                table: "TouristsTours",
+                keyColumn: "Id",
+                keyValue: new Guid("f4e0c782-d1a4-42d1-9182-90b6fb2935e4"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 31, 23, 15, 54, 178, DateTimeKind.Utc).AddTicks(1577));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TouristsTours",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 3, 31, 23, 0, 27, 398, DateTimeKind.Utc).AddTicks(5137),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 3, 31, 23, 15, 54, 178, DateTimeKind.Utc).AddTicks(1577));
+
+            migrationBuilder.UpdateData(
+                table: "TouristsTours",
+                keyColumn: "Id",
+                keyValue: new Guid("f4e0c782-d1a4-42d1-9182-90b6fb2935e4"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 31, 23, 0, 27, 398, DateTimeKind.Utc).AddTicks(5137));
+        }
+    }
+}
