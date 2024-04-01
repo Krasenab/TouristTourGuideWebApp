f2121fd3d733e9bba2b8fff1e5756b8b413dadda
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2024-04-01 04:08:42 +0300

    Add Vote Coments , add column in TouristTours migration

---
+++ TouristTourGuide.Data/Migrations/20240401010727_AddNewTableAndColumns.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TouristTourGuide.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewTableAndColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TouristsTours_Categories_CategoryId",
+                table: "TouristsTours");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TouristsTours_Locations_LocationId",
+                table: "TouristsTours");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WhatToBring",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NotSuitableFor",
+                table: "TouristsTours",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MeetingPointMapUrl",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KnowBeforeYouGo",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TouristsTours",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 4, 1, 1, 7, 26, 830, DateTimeKind.Utc).AddTicks(6467),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Highlights",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Includes",
+                table: "TouristsTours",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegisteredAddress",
+                table: "GuideUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyRegistrationNumber",
+                table: "GuideUsers",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "GuideUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    TouristTourId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_TouristsTours_TouristTourId",
+                        column: x => x.TouristTourId,
+                        principalTable: "TouristsTours",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vote",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VoteValue = table.Column<int>(type: "int", maxLength: 5, nullable: false),
+                    ApplicationUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TouristTourId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vote", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vote_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vote_TouristsTours_TouristTourId",
+                        column: x => x.TouristTourId,
+                        principalTable: "TouristsTours",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "Id", "City", "Country", "Village" },
+                values: new object[] { 1, "London", "UK", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_TouristTourId",
+                table: "Comments",
+                column: "TouristTourId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vote_ApplicationUserId",
+                table: "Vote",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vote_TouristTourId",
+                table: "Vote",
+                column: "TouristTourId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TouristsTours_Categories_CategoryId",
+                table: "TouristsTours",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TouristsTours_Locations_LocationId",
+                table: "TouristsTours",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TouristsTours_Categories_CategoryId",
+                table: "TouristsTours");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TouristsTours_Locations_LocationId",
+                table: "TouristsTours");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Vote");
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "Highlights",
+                table: "TouristsTours");
+
+            migrationBuilder.DropColumn(
+                name: "Includes",
+                table: "TouristsTours");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "GuideUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WhatToBring",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NotSuitableFor",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MeetingPointMapUrl",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KnowBeforeYouGo",
+                table: "TouristsTours",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TouristsTours",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 4, 1, 1, 7, 26, 830, DateTimeKind.Utc).AddTicks(6467));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegisteredAddress",
+                table: "GuideUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyRegistrationNumber",
+                table: "GuideUsers",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TouristsTours_Categories_CategoryId",
+                table: "TouristsTours",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TouristsTours_Locations_LocationId",
+                table: "TouristsTours",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+    }
+}
