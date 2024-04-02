5cc8afc691fa39f15f7c57e7764aa6de0dc09615
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2024-04-03 00:31:12 +0300

    Add create tour form view, add tour controller

---
+++ TouristTourGuideWebApp/Controllers/TouristTourController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TouristTourGuide.Services;
+using TouristTourGuide.Services.Interfaces;
+
+namespace TouristTourGuideWebApp.Controllers
+{
+    public class TouristTourController : Controller
+    {
+        private TourService _tourService;
+        
+        public TouristTourController(TourService tourService)
+        {
+                this._tourService = tourService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
