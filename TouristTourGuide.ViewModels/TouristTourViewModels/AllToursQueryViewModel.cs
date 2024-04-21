543a5019d1381cd43bbf4f117c11aab92855c79a
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2024-04-21 03:59:30 +0300

    add all view

--- TouristTourGuide.ViewModels/TouristTourViewModels/AllToursQueryViewModel.cs
+++ TouristTourGuide.ViewModels/TouristTourViewModels/AllToursQueryViewModel.cs
@@ -7,24 +7,21 @@ namespace TouristTourGuide.ViewModels.TouristTourViewModels
     {
         public AllToursQueryViewModel()
         {
-                
-        }
+            this.Tours = new List<AllViewModel>();
 
-        public string Category { get; set; }
+            this.Categories = new List<string>();
+        }
+        public string? Category { get; set; }
+
         [Display(Name = "Serching by word")]
         public string? SerchByString { get; set; }
 
         [Display(Name = "Sort tour by")]
         public TourSorting TourSorting { get; set; }
-
         public int CurrentPage { get; set; } = 1;
-
         public int TourPerPage { get; set; } = 3;
-
         public int TourTotal { get; set; }
-
         public List<string> Categories { get; set; }
-
         public List<AllViewModel> Tours { get; set; }
     }
 }
